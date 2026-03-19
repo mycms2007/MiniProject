4c7b40c4df76de75362b6d596138b4c9aeaca79a
mycms2007 <mycms2007@gmail.com>
2026-03-20 03:06:44 +0900

    Add files via upload

---
+++ BattlePrototype Scripts/JSONParser.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class JSONParser : MonoBehaviour
+{
+    public MonsterData[] monsters;
+
+    void Start()
+    {
+        monsters = ParseJSON("monsters");
+        foreach (MonsterData monster in monsters)
+        {
+            Debug.Log($"[JSON] {monster.monsterName} / HP:{monster.hp} / 공격:{monster.attack} / 방어:{monster.defense} / 드랍:{monster.dropItem}({monster.dropRate * 100}%)");
+        }
+    }
+
+    public MonsterData[] ParseJSON(string fileName)
+    {
+        TextAsset jsonFile = Resources.Load<TextAsset>(fileName);
+        MonsterDataList dataList = JsonUtility.FromJson<MonsterDataList>(jsonFile.text);
+
+        MonsterData[] result = new MonsterData[dataList.monsters.Length];
+
+        for (int i = 0; i < dataList.monsters.Length; i++)
+        {
+            MonsterData data = ScriptableObject.CreateInstance<MonsterData>();
+            data.monsterName = dataList.monsters[i].monsterName;
+            data.hp = dataList.monsters[i].hp;
+            data.attack = dataList.monsters[i].attack;
+            data.defense = dataList.monsters[i].defense;
+            data.dropItem = dataList.monsters[i].dropItem;
+            data.dropRate = dataList.monsters[i].dropRate;
+            result[i] = data;
+        }
+
+        return result;
+    }
+}
+
+[System.Serializable]
+public class MonsterDataRaw
+{
+    public string monsterName;
+    public int hp;
+    public int attack;
+    public int defense;
+    public string dropItem;
+    public float dropRate;
+}
+
+[System.Serializable]
+public class MonsterDataList
+{
+    public MonsterDataRaw[] monsters;
+}
